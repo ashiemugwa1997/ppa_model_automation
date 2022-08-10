2a24897f46b4ae7acde4b22f4bc4fa09a49fb603
ashley mugwambi <ashleyzarter@gmail.com>
2022-08-10 15:37:49 +0200

    assumptions model

--- ppa_model_automation/ppa_model/admin.py
+++ ppa_model_automation/ppa_model/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Upload_Doc
+from .models import Upload_Doc, assumptions
 
 # Register your models here.
 admin.site.register(Upload_Doc)
+admin.site.register(assumptions)
--- ppa_model_automation/ppa_model/models.py
+++ ppa_model_automation/ppa_model/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Upload_Doc(models.Model):
-    file = models.FileField(upload_to='uploads/')
+    file = models.FileField(upload_to='ppa_model/uploads/')
     name_of_upload = models.CharField(max_length=300)
     class_of_business = models.CharField(max_length=300)
     name_of_policyholder = models.CharField(max_length=300)
@@ -19,5 +19,24 @@ class Upload_Doc(models.Model):
 
     def __str__(self):
         return self.name_of_upload
+
+    objects = models.Manager()
+
+
+class assumptions(models.Model):
+    name_of_assumption = models.CharField(max_length=300)
+    updated = models.DateTimeField(auto_now=True, blank=True)
+    user = models.CharField(max_length=100)
+    class_of_business = models.CharField(max_length=100)
+    discount_rate = models.FloatField()
+    expense_ratio = models.FloatField()
+    loss_ratio = models.FloatField()
+    risk_adjustment = models.FloatField()
+    acquisition_costs = models.FloatField()
+    id = models.AutoField(primary_key=True)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name_of_assumption
 
     objects = models.Manager()
