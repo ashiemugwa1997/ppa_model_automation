4f58cffc34efe968f2bedc1ba592f373ba5cf825
ashley mugwambi <ashleyzarter@gmail.com>
2022-08-12 16:54:32 +0200

    assumptions model

--- ppa_model_automation/ppa_model/models.py
+++ ppa_model_automation/ppa_model/models.py
@@ -2,20 +2,10 @@ from django.db import models
 
 
 class Upload_Doc(models.Model):
-    file = models.FileField(upload_to='ppa_model/uploads/')
-    name_of_upload = models.CharField(max_length=300)
-    class_of_business = models.CharField(max_length=300)
-    name_of_policyholder = models.CharField(max_length=300)
-    surname = models.CharField(max_length=300)
-    policy_number = models.CharField(max_length=300)
-    start_date = models.DateField()
-    ending_date = models.DateField()
-    expected_date_of_premium_payment = models.DateField()
-    date_of_premium_payment = models.DateField()
-    premium_installment = models.FloatField()
-    payment_frequency = models.IntegerField()
-    total_premium = models.FloatField()
+    name_of_file = models.CharField(max_length=400)
+    name_of_assumption_and_datasheet = models.CharField(max_length=300)
     updated = models.DateTimeField(auto_now=True, blank=True)
+    name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.name_of_upload
@@ -24,7 +14,7 @@ class Upload_Doc(models.Model):
 
 
 class assumptions(models.Model):
-    name_of_assumption = models.CharField(max_length=300)
+    name_of_assumption_and_datasheet = models.CharField(max_length=300)
     updated = models.DateTimeField(auto_now=True, blank=True)
     user = models.CharField(max_length=100)
     class_of_business = models.CharField(max_length=100)
